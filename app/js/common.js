2e53cd47bdfb87e79f452f3d7655147bb63734e6
Yuriy Bahur <yuriybahur1@gmail.com>
2018-10-11 01:30:43 +0300

    Правки

--- app/js/common.js
+++ app/js/common.js
@@ -1,117 +1,116 @@
+$(function () {
 
-$(function() {
-	$(window).scroll( () => {
+	$(window).scroll(function () {
 		var windowTop = $(window).scrollTop();
-		windowTop > 100 ? $('nav').addClass('navShadow') : $('nav').removeClass('navShadow');
-		windowTop > 100 ? $('ul').css('top','100px') : $('ul').css('top','160px');
-		if(screen.width > 900){
-		windowTop > 100 ? $('#logo').css('width','200px') : $('#logo').css('width','242px');
-		
-		windowTop > 100 ? $('#slogan').removeClass('sloganShadow') : $('#slogan').addClass('sloganShadow');
-		windowTop > 100 ? $('#slogan').css('font-size','14px') : $('#slogan').css('font-size','18px');
-	}
-		
+		windowTop >= 100 ? $('nav').addClass('navShadow') : $('nav').removeClass('navShadow');
+		windowTop >= 100 ? $('ul').css('top', '100px') : $('ul').css('top', '160px');
+		if (screen.width > 900) {
+			windowTop >= 100 ? $('#logo').css('width', '200px') : $('#logo').css('width', '242px');
+
+			windowTop >= 100 ? $('#slogan').removeClass('sloganShadow') : $('#slogan').addClass('sloganShadow');
+			windowTop >= 100 ? $('#slogan').css('font-size', '14px') : $('#slogan').css('font-size', '18px');
+		}
+
 	});
-	
+
 	//Click Logo To Scroll To Top
-	$('#logo').on('click', () => {
+	$('#logo').on('click', function () {
 		$('html,body').animate({
 			scrollTop: 0
-		},500);
+		}, 500);
 	});
-	
-		
 
 
-	$(document).ready(function() {
-		  $('#accordeon .acc-head').on('click', f_acc);	  
-	  });	  
-	  function f_acc(){
+	$(document).ready(function () {
+		$('#accordeon .acc-head').on('click', f_acc);
+	});
 
-      var th = $(this),
+	function f_acc() {
+
+		var th = $(this),
 			item = th.closest('.ack-adv-tabs'),
 			itemMaxHeight = 0;
 
-            item.toggleClass('is-active').siblings().removeClass('is-active');
+		item.toggleClass('is-active').siblings().removeClass('is-active');
 
-			th.next().slideToggle('fast').addClass('is-opened').closest(item).siblings().find('.is-opened').slideUp('fast').removeClass('is-opened');
+		th.next().slideToggle('fast').addClass('is-opened').closest(item).siblings().find('.is-opened').slideUp('fast').removeClass('is-opened');
 
-			item.siblings().each(function () { 
+		item.siblings().each(function () {
 
-				if ( $(this).outerHeight() > itemMaxHeight ) {
+			if ($(this).outerHeight() > itemMaxHeight) {
 
-					itemMaxHeight = $(this).outerHeight();
+				itemMaxHeight = $(this).outerHeight();
 
-				} 
+			}
 
-			 });
-
-			if ( $(window).width() < 993 ) {
-
-				$('html,body').animate({
-					scrollTop: $(this).offset().top - itemMaxHeight - 100
-				}, 200);
-
-			} 
-
-     }
-
-		var list = $(".img-h");
-		$(list).on('mouseenter', function () {
-			$(list).toggleClass('filter-img');
-			$(this).removeClass('filter-img');
-		}).on('mouseleave', function() {
-			$(list).removeClass('filter-img');
 		});
 
+		if ($(window).width() < 993) {
+
+			$('html,body').animate({
+				scrollTop: $(this).offset().top - itemMaxHeight - 100
+			}, 200);
+
+		}
+
+	}
+
+	var list = $(".img-h");
+	$(list).on('mouseenter', function () {
+		$(list).toggleClass('filter-img');
+		$(this).removeClass('filter-img');
+	}).on('mouseleave', function () {
+		$(list).removeClass('filter-img');
+	});
 
 
-		$('.owl-carousel').owlCarousel({
-			loop:false,
-		margin:10,
-		merge:true,
-	responsive:{
-1000:{
-			items:4,
-			mouseDrag: false,
-			touchDrag: true,
-			loop:false,
-		},
-		0:{
-		items:1,
-		
-        },
-    }
-});
+
+	$('.owl-carousel').owlCarousel({
+		loop: false,
+		margin: 10,
+		merge: true,
+		responsive: {
+			1000: {
+				items: 4,
+				mouseDrag: false,
+				touchDrag: true,
+				loop: false,
+			},
+			0: {
+				items: 1,
+
+			},
+		}
+	});
 
 
 	function waypoints() {
 
 		var slogan = $("#slogan"),
-			waypoint = $('[data-waypoint]'); 
-		
-		if ( $(window).width() < 992 ) {
+			waypoint = $('[data-waypoint]');
 
-			$(window).on('scroll', function() {
+		if ($(window).width() < 992) {
 
-				slogan.text( 'Сложно, дорого, но помочь можно' );
-	
+			$(window).on('scroll', function () {
+
+				slogan.text('Сложно, дорого, но помочь можно');
+
 				waypoint.each(function () {
-	
-					if ( $(window).scrollTop() >= $(this).offset().top - 135 && $(window).scrollTop() < $(this).offset().top + $(this).outerHeight() ) {
-	
-						slogan.text( $(this).attr('data-text') );
-		
-					} 
 
-					if($(window).scrollTop() + $(window).height() > $(document).height() - 40) {
+					if ($(window).scrollTop() >= $(this).offset().top - 135 && $(window).scrollTop() < $(this).offset().top + $(this).outerHeight()) {
 
-						slogan.text( 'Сложно, дорого, но помочь можно' );
+						slogan.text($(this).attr('data-text'));
 
 					}
-	
+
+					if ($(window).scrollTop() + $(window).height() > $(document).height() - 60) {
+
+						slogan.text('Сложно, дорого, но помочь можно');
+
+					}
+
 				});
-	
+
 			});
 
 		}
@@ -119,12 +118,12 @@ $(function() {
 	}
 
 	waypoints();
-		  
+
 
 	$('.box-modal').on('click', function () {
 
 		$.arcticmodal('close');
-		
+
 	});
 
 
